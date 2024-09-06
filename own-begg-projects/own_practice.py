6d6a1fbabca5ea1d6c60c809017eda0181fbc7b1
Abdiel Murillo <abdielmisael09@gmail.com>
2024-09-05 19:36:42 -0500

    Update own_practice.py
    add "naranja" to the program

--- own-begg-projects/own_practice.py
+++ own-begg-projects/own_practice.py
@@ -1,6 +1,6 @@
-frutas = ["banana","sandia","pera","granada","uva","cereza","fresa"]
+frutas = ["banana","sandia","pera","granada","uva","cereza","fresa","naranja"]
 frutas_llevar = []
-precio = [2,4,3.5,1.32,4.21,3.5,7]
+precio = [2,4,3.5,1.32,4.21,3.5,7,6.95]
 frutas_precios = dict(zip(frutas,precio)) #zip lo que hace es: oma dos (o más) iterables (como listas) y los combina en pares. Cada par contiene un elemento de cada iterable en la misma posición.
 print (frutas_precios)
 
